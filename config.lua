82b9a9e3ebbca58d19d5f2c9eb2f950cba787d9e
Christian Chiarulli <chris.machine@pm.me>
2022-09-25 16:45:12 -0400

    chore: cleanup

--- config.lua
+++ config.lua
@@ -34,17 +34,3 @@ lvim.plugins = {
 }
 
 require("user.dap-ui")
-
--- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- vim.api.nvim_create_autocmd("BufEnter", {
---   pattern = { "*.json", "*.jsonc" },
---   -- enable wrap mode for json files only
---   command = "setlocal wrap",
--- })
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = "zsh",
---   callback = function()
---     -- let treesitter use bash highlight for zsh files as well
---     require("nvim-treesitter.highlight").attach(0, "bash")
---   end,
--- })
