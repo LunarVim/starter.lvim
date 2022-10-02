f52cdf2b903463996aeaca80f65dca329442b317
RitchielRez <ritchielrez@gmail.com>
2022-10-03 02:18:09 +0600

    feat: go ide setup

---
+++ config.lua
@@ -0,0 +1,45 @@
+lvim.log.level = "warn"
+lvim.format_on_save = true
+lvim.colorscheme = "tokyonight"
+
+-- to disable icons and use a minimalist setup, uncomment the following
+-- lvim.use_icons = false
+
+-- keymappings [view all the defaults by pressing <leader>Lk]
+lvim.leader = "space"
+
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.notify.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.breadcrumbs.active = true
+lvim.builtin.treesitter.highlight.enabled = true
+lvim.builtin.dap.active = true
+-- lvim.builtin.terminal.shell = "pwsh.exe -NoLogo"
+
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = {
+  "go",
+}
+lvim.builtin.treesitter.highlight.enable = true
+
+-- set a formatter, this will override the language server formatting capabilities (if it exists)
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  { command = "goimports", filetypes = { "go" } },
+}
+
+local lsp_manager = require "lvim.lsp.manager"
+lsp_manager.setup("gopls")
+
+-- Additional Plugins
+lvim.plugins = {
+  "rcarriga/nvim-dap-ui",
+  "leoluz/nvim-dap-go",
+}
+
+require("user.dap-go")
+require("user.dap-ui")
