7edc6f06178d9f19d298a8dce3e40b2334f85542
0x736d <subash.mahapatra@proton.me>
2022-10-03 03:38:50 +0530

    feat: setup lsp

---
+++ config.lua
@@ -0,0 +1,44 @@
+lvim.log.level = "warn"
+lvim.format_on_save = false
+lvim.leader = "space"
+lvim.lsp.diagnostics.virtual_text = true
+
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.notify.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.breadcrumbs.active = true
+lvim.builtin.treesitter.highlight.enable = true
+-- enable dap
+lvim.builtin.dap.active = true
+
+-- auto install treesitter parsers
+lvim.builtin.treesitter.ensure_installed = { "cpp", "c" }
+
+
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "clangd" })
+
+-- setup clangd
+
+-- customize clangd by changing the flags below
+local clangd_flags = {
+  "--background-index",
+  "--fallback-style=google",
+  "--all-scopes-completion",
+  "--clang-tidy",
+  "--log=error",
+  "--completion-style=detailed",
+  "--pch-storage=disk",
+  "--folding-ranges",
+  "--enable-config",
+}
+
+
+require("lvim.lsp.manager").setup("clangd", {
+  cmd = { "clangd", unpack(clangd_flags) },
+  on_attach = require("lvim.lsp").common_on_attach,
+  on_init = require("lvim.lsp").common_on_init,
+  capabilities = require("lvim.lsp").common_capabilities()
+})
