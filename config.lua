272eadbe56bb5d06b418beff49dd8a690ba6d9ec
Hashem A. Damrah <singularisartt@gmail.com>
2022-10-04 12:36:57 -0700

    add dap-python configuration

--- config.lua
+++ config.lua
@@ -43,14 +43,39 @@ linters.setup {
   { command = "flake8", filetypes = { "python" } },
 }
 
-
 -- Setup dap for python
-local dap = require("dap")
+local dap = require "dap"
 
 dap.adapters.python = {
   type = "executable",
-  command = "/usr/bin/python3.10",
+  command = "/usr/bin/python",
   args = { "-m", "debugpy.adapter" },
+}
+
+dap.configurations.python = {
+  {
+    -- The first three options are required by nvim-dap
+    type = "python", -- the type here established the link to the adapter definition: `dap.adapters.python`
+    request = "launch",
+    name = "Launch file",
+
+    -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+    program = "${file}", -- This configuration will launch the current file if used.
+    pythonPath = function()
+      -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+      -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+      -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+      local cwd = vim.fn.getcwd()
+      if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+        return cwd .. "/venv/bin/python"
+      elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+        return cwd .. "/.venv/bin/python"
+      else
+        return "/usr/bin/python"
+      end
+    end,
+  },
 }
 
 -- Additional Plugins
