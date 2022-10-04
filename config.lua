be6abb93710fd39735ef88537014e99b1fc32227
0x736d <subash.mahapatra@proton.me>
2022-10-04 20:23:20 +0530

    feat: add manual configurations for language servers
    manually configured servers
    
    - rust_analyzer
    
    - clangd
    
    - gopls

--- config.lua
+++ config.lua
@@ -69,6 +69,84 @@ lvim.lsp.installer.setup.ensure_installed = {
   "taplo",
 }
 
+-- language servers added to the skipped_servers table will not be -
+-- automatically configured by LunarVim
+
+-- Add servers to the skipped_servers table when manual configuration
+-- of a language server is needed
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "gopls", "rust_analyzer", "clangd" })
+
+-- See below the examples of manual configuration of `gopls`, `clangd` -
+-- and `rust_analyzer`
+------------------------------------------------------------------------
+-- Manual setup of language servers
+------------------------------------------------------------------------
+
+local lsp_manager = require "lvim.lsp.manager"
+local on_init = require("lvim.lsp").common_on_init
+local on_attach = require("lvim.lsp").common_on_attach
+local capabilities = require("lvim.lsp").common_capabilities()
+
+-- clangd
+local clangd_flags = {
+  "--offset-encoding=utf-16",
+  "--fallback-style=google",
+  "--enable-config",
+  "--clang-tidy",
+}
+
+local clangd_capabilities = capabilities
+clangd_capabilities.offsetEncoding = { "utf-16" }
+
+-- clangd
+lsp_manager.setup("clangd", {
+  cmd = { "clangd", unpack(clangd_flags) },
+  on_init = on_init,
+  on_attach = on_attach,
+  capabilities = clangd_capabilities,
+})
+
+-- rust_analyzer
+lsp_manager.setup("rust_analyzer", {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  settings = {
+    ["rust-analyzer"] = {
+      checkOnSave = {
+        enable = true,
+        command = "clippy",
+      },
+    },
+  },
+})
+
+-- gopls
+lsp_manager.setup("gopls", {
+  settings = {
+    gopls = {
+      gofumpt = true,
+      codelenses = {
+        gc_details = true,
+        generate = true,
+        test = true,
+        tidy = true,
+        upgrade_dependency = true,
+      },
+      usePlaceholders = true,
+      analyses = {
+        fieldalignment = true,
+        nilness = true,
+        shadow = true,
+        unusedparams = true,
+        unusedwrite = true,
+      },
+      staticcheck = true,
+    },
+  },
+})
+
+
 
 
 ------------------------------------------------------------------------
