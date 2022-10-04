89b660c393e62e44cd695e86659158ae0d7528e4
0x736d <subash.mahapatra@proton.me>
2022-10-04 19:53:25 +0530

    feat: auto install treesitter parsers & some lsp servers

--- config.lua
+++ config.lua
@@ -19,3 +19,44 @@ lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 lvim.builtin.treesitter.highlight.enable = true
 lvim.builtin.breadcrumbs.active = true
+
+
+------------------------------------------------------------------------
+-- auto install treesitter parsers
+------------------------------------------------------------------------
+lvim.builtin.treesitter.ensure_installed = {
+  "c",
+  "cpp",
+  "javascript",
+  "typescript",
+  "tsx",
+  "json",
+  "lua",
+  "html",
+  "python",
+  "css",
+  "scss",
+  "yaml",
+  "toml",
+  "go",
+  "bash",
+  "dockerfile",
+  "rust",
+}
+
+
+------------------------------------------------------------------------
+-- LSP settings
+------------------------------------------------------------------------
+-- language server in the table below will automatically be installed
+lvim.lsp.installer.setup.ensure_installed = {
+  "sumeko_lua",
+  "jsonls",
+  "pyright",
+  "tsserver",
+  "html",
+  "yamlls",
+  "dockerls",
+  "bashls",
+  "taplo",
+}
