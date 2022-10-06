a49825e0eafd3137d54b7041e9013ea7281a8a76
Vitor Faiante <vitorf7@gmail.com>
2022-10-07 00:59:48 +0100

    Apply stream feedback

--- config.lua
+++ config.lua
@@ -1,42 +1,88 @@
--- ===================================== General Section ============================================
-lvim.log.level = "warn"
-lvim.colorscheme = "tokyonight"
-lvim.leader = "space"
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
-lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
-lvim.builtin.breadcrumbs.active = true
-
-
--- =================================== Treesitter Section ===========================================
+----------------------
+-- Treesitter
+----------------------
 lvim.builtin.treesitter.ensure_installed = {
   "go",
-}
-lvim.builtin.treesitter.highlight.enable = true
-
-
--- ======================================= Dap Section ==============================================
-lvim.builtin.dap.active = true
-
-
--- ===================================== Plugins Section ============================================
-lvim.plugins = {}
-
-
--- ================================= Go IDE Specific Section ========================================
-GOIDE_CONFIG = {
-  use_simple = true,
-  format_on_save = false
+  "gomod",
 }
 
-lvim.format_on_save = false
-local goide = require("user.goide_simple")
-if not GOIDE_CONFIG.use_simple then
-  goide = require("user.goide_complex")
+----------------------
+-- Plugins
+----------------------
+lvim.plugins = {
+  "olexsmir/gopher.nvim",
+  "leoluz/nvim-dap-go",
+}
+
+----------------------
+-- Formating
+----------------------
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  { command = "goimports", filetypes = { "go" } },
+  { command = "gofumpt", filetypes = { "go" } },
+  { command = "stylua", filetypes = { "lua" } },
+}
+
+lvim.format_on_save = {
+  pattern = { "*.go" },
+}
+
+----------------------
+-- LSP
+----------------------
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "gopls" })
+
+local lsp_manager = require "lvim.lsp.manager"
+lsp_manager.setup("golangci_lint_ls", {
+  on_init = require("lvim.lsp").common_on_init,
+  capabilities = require("lvim.lsp").common_capabilities(),
+})
+
+lsp_manager.setup("gopls", {
+  on_attach = function(client, bufnr)
+    require("lvim.lsp").common_on_attach(client, bufnr)
+    local _, _ = pcall(vim.lsp.codelens.refresh)
+  end,
+  on_init = require("lvim.lsp").common_on_init,
+  capabilities = require("lvim.lsp").common_capabilities(),
+  settings = {
+    gopls = {
+      usePlaceholders = true,
+      gofumpt = true,
+      codelenses = {
+        generate = false,
+        gc_details = true,
+        test = true,
+        tidy = true,
+      },
+    },
+  },
+})
+
+local status_ok, gopher = pcall(require, "gopher")
+if not status_ok then
+  return
 end
 
-goide.start()
+gopher.setup {
+  commands = {
+    go = "go",
+    gomodifytags = "gomodifytags",
+    gotests = "gotests",
+    impl = "impl",
+    iferr = "iferr",
+  },
+}
+
+----------------------
+-- Dap
+----------------------
+local dap_ok, dapgo = pcall(require, "dap-go")
+if not dap_ok then
+  return
+end
+
+dapgo.setup()
+
+lvim.builtin.which_key.mappings["dT"] = { "<cmd>lua require('dap-go').debug_test()<cr>", "Debug Test" }
