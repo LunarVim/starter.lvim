ee4579a1c8b6d12a962dee4417e14c3ba7591aff
0x736d <subash.mahapatra@proton.me>
2022-10-07 00:28:43 +0530

    fix: remove unnecessary lunarvim opts

--- config.lua
+++ config.lua
@@ -1,31 +1,12 @@
-lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.leader = "space"
-
--- uncomment the line below to disable icons
--- lvim.use_icons = false
-
 ------------------------------------------------------------------------
 -- colorscheme
 -- choose from "tokyonight", "catppuccin", "kanagawa"
-------------------------------------------------------------------------
 lvim.colorscheme = "tokyonight"
-
 ------------------------------------------------------------------------
--- builtin plugins configs
 
--- After changing plugin config exit and reopen LunarVim -
--- Run :PackerInstall :PackerCompile
-------------------------------------------------------------------------
-lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
-lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 lvim.builtin.treesitter.highlight.enable = true
 lvim.builtin.treesitter.autotag.enable = true
-lvim.builtin.breadcrumbs.active = true
 
 ------------------------------------------------------------------------
 -- auto install treesitter parsers
