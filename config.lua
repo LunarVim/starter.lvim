055a39cbbb2afdc181f2471c7e7e757c4e65117e
Nick Waywood <nick.waywood@zendesk.com>
2022-10-06 17:52:40 +1100

    Formatting

--- config.lua
+++ config.lua
@@ -16,24 +16,23 @@ end
 -- status bar https://github.com/scalameta/nvim-metals/blob/main/doc/metals.txt#L101-L114
 local components = require("lvim.core.lualine.components")
 lvim.builtin.lualine.sections.lualine_c = {
-  -- NOTE: There is no way to append a component, so we need to include the components
-  -- here that are already supplied by lunarvim in `lualine_c`
+	-- NOTE: There is no way to append a component, so we need to include the components
+	-- here that are already supplied by lunarvim in `lualine_c`
 	components.diff,
 	components.python_env,
 	metals_status,
 }
 
-
 lvim.builtin.which_key.mappings["m"] = {
-  name = "Metals",
-  u = { "<Cmd>MetalsUpdate<CR>", "Update Metals" },
-  i = { "<Cmd>MetalsInfo<CR>", "Metals Info" },
-  r = { "<Cmd>MetalsRestartBuild<CR>", "Restart Build Server" },
-  d = { "<Cmd>MetalsRunDoctor<CR>", "Metals Doctor" },
+	name = "Metals",
+	u = { "<Cmd>MetalsUpdate<CR>", "Update Metals" },
+	i = { "<Cmd>MetalsInfo<CR>", "Metals Info" },
+	r = { "<Cmd>MetalsRestartBuild<CR>", "Restart Build Server" },
+	d = { "<Cmd>MetalsRunDoctor<CR>", "Metals Doctor" },
 }
 
 lvim.plugins = {
-  "scalameta/nvim-metals",
+	"scalameta/nvim-metals",
 }
 
 -- Setting up the lsp initialisation in an autocmd
@@ -41,15 +40,15 @@ local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = t
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
 	pattern = { "*.scala", "*.sbt", "*.sc" },
 	callback = function()
-    local metals_config = require("metals").bare_config()
-    metals_config.on_attach = require("lvim.lsp").common_on_attach
-    metals_config.init_options.statusBarProvider = "on"
-    metals_config.settings = {
-    	showImplicitArguments = false,
-    	showInferredType = true,
-    	excludedPackages = {},
-    }
-    require("metals").initialize_or_attach(metals_config)
+		local metals_config = require("metals").bare_config()
+		metals_config.on_attach = require("lvim.lsp").common_on_attach
+		metals_config.init_options.statusBarProvider = "on"
+		metals_config.settings = {
+			showImplicitArguments = false,
+			showInferredType = true,
+			excludedPackages = {},
+		}
+		require("metals").initialize_or_attach(metals_config)
 	end,
 	group = nvim_metals_group,
 })
