d9376fd036ce658cb01ac42525573b30c1deff83
Daniel Tingelöf <daniel.tingelof@gmail.com>
2022-10-17 19:59:07 +0200

    fix: simplify keymaps

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -20,54 +20,6 @@ elseif vim.fn.has("unix") == 1 then
 else
 	print("Unsupported system")
 end
-local function set_keymaps(buffer)
-	local status_ok, which_key = pcall(require, "which-key")
-	if not status_ok then
-		return
-	end
-
-	local opts = {
-		mode = "n", -- NORMAL mode
-		prefix = "<leader>",
-		buffer = buffer, -- Global mappings. Specify a buffer number for buffer local mappings
-		silent = true, -- use `silent` when creating keymaps
-		noremap = true, -- use `noremap` when creating keymaps
-		nowait = true, -- use `nowait` when creating keymaps
-	}
-
-	local vopts = {
-		mode = "v", -- VISUAL mode
-		prefix = "<leader>",
-		buffer = buffer, -- Global mappings. Specify a buffer number for buffer local mappings
-		silent = true, -- use `silent` when creating keymaps
-		noremap = true, -- use `noremap` when creating keymaps
-		nowait = true, -- use `nowait` when creating keymaps
-	}
-
-	local mappings = {
-		L = {
-			name = "Java",
-			o = { "<Cmd>lua require'jdtls'.organize_imports()<CR>", "Organize Imports" },
-			v = { "<Cmd>lua require('jdtls').extract_variable()<CR>", "Extract Variable" },
-			c = { "<Cmd>lua require('jdtls').extract_constant()<CR>", "Extract Constant" },
-			t = { "<Cmd>lua require'jdtls'.test_nearest_method()<CR>", "Test Method" },
-			T = { "<Cmd>lua require'jdtls'.test_class()<CR>", "Test Class" },
-			u = { "<Cmd>JdtUpdateConfig<CR>", "Update Config" },
-		},
-	}
-
-	local vmappings = {
-		L = {
-			name = "Java",
-			v = { "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>", "Extract Variable" },
-			c = { "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>", "Extract Constant" },
-			m = { "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", "Extract Method" },
-		},
-	}
-
-	which_key.register(mappings, opts)
-	which_key.register(vmappings, vopts)
-end
 
 -- Find root of project
 local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
@@ -244,9 +196,24 @@ local config = {
 config["on_attach"] = function(client, bufnr)
 	local _, _ = pcall(vim.lsp.codelens.refresh)
 	require("jdtls.dap").setup_dap_main_class_configs()
-	require("jdtls").setup_dap({ hotcodereplace = "auto" })
+	jdtls.setup_dap({ hotcodereplace = "auto" })
 	require("lvim.lsp").on_attach(client, bufnr)
-	set_keymaps(bufnr)
+	local map = function(mode, lhs, rhs, desc)
+		if desc then
+			desc = desc
+		end
+
+		vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc, buffer = bufnr, noremap = true })
+	end
+	map("n", "<leader>Co", jdtls.organize_imports(), "Organize Imports")
+	map("n", "<leader>Cv", jdtls.extract_variable(), "Extract Variable")
+	map("n", "<leader>Cc", jdtls.extract_constant(), "Extract Constant")
+	map("n", "<leader>Ct", jdtls.test_nearest_method(), "Test Method")
+	map("n", "<leader>CT", jdtls.test_class(), "Test Class")
+	map("n", "<leader>Cu", "<Cmd>JdtUpdateConfig<CR>", "Update Config")
+	map("v", "<leader>Cv", "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>", "Extract Variable")
+	map("v", "<leader>Cc", "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>", "Extract Constant")
+	map("v", "<leader>Cm", "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", "Extract Method")
 end
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
