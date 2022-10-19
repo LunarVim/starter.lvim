ddb42ebb614c3ceb2387ffd02d9eb70011ff8138
kylo252 <59826753+kylo252@users.noreply.github.com>
2022-10-19 15:37:57 +0200

    feat(c-ide): add clangd-extensions

--- config.lua
+++ config.lua
@@ -6,32 +6,76 @@ lvim.builtin.treesitter.highlight.enable = true
 -- auto install treesitter parsers
 lvim.builtin.treesitter.ensure_installed = { "cpp", "c" }
 
-vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "clangd" })
--- setup clangd
+-- Additional Plugins
+table.insert(lvim.plugins, {
+  "p00f/clangd_extensions.nvim",
+})
 
--- customize clangd by changing the flags below
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "clangd" })
+
+-- some settings can only passed as commandline flags, see `clangd --help`
 local clangd_flags = {
   "--background-index",
-  "--fallback-style=google",
+  "--fallback-style=Google",
   "--all-scopes-completion",
   "--clang-tidy",
   "--log=error",
+  "--suggest-missing-includes",
+  "--cross-file-rename",
   "--completion-style=detailed",
-  "--pch-storage=disk",
+  "--pch-storage=memory", -- could also be disk
   "--folding-ranges",
-  "--enable-config",
-  "--offset-encoding=utf-16",
+  "--enable-config", -- clangd 11+ supports reading from .clangd configuration file
+  "--offset-encoding=utf-16", --temporary fix for null-ls
+  -- "--limit-references=1000",
+  -- "--limit-resutls=1000",
+  -- "--malloc-trim",
+  -- "--clang-tidy-checks=-*,llvm-*,clang-analyzer-*,modernize-*,-modernize-use-trailing-return-type",
+  -- "--header-insertion=never",
+  -- "--query-driver=<list-of-white-listed-complers>"
 }
 
-local capabilities = require("lvim.lsp").common_capabilities()
-capabilities.offsetEncoding = { "utf-16" }
+local provider = "clangd"
 
-require("lvim.lsp.manager").setup("clangd", {
-  cmd = { "clangd", unpack(clangd_flags) },
-  on_attach = require("lvim.lsp").common_on_attach,
-  on_init = require("lvim.lsp").common_on_init,
-  capabilities = capabilities,
-})
+local custom_on_attach = function(client, bufnr)
+  require("lvim.lsp").common_on_attach(client, bufnr)
+
+  local opts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set("n", "<leader>lh", "<cmd>ClangdSwitchSourceHeader<cr>", opts)
+  vim.keymap.set("x", "<leader>lA", "<cmd>ClangdAST<cr>", opts)
+  vim.keymap.set("n", "<leader>lH", "<cmd>ClangdTypeHierarchy<cr>", opts)
+  vim.keymap.set("n", "<leader>lt", "<cmd>ClangdSymbolInfo<cr>", opts)
+  vim.keymap.set("n", "<leader>lm", "<cmd>ClangdMemoryUsage<cr>", opts)
+
+  require("clangd_extensions.inlay_hints").setup_autocmd()
+  require("clangd_extensions.inlay_hints").set_inlay_hints()
+end
+
+local status_ok, project_config = pcall(require, "rhel.clangd_wrl")
+if status_ok then
+  clangd_flags = vim.tbl_deep_extend("keep", project_config, clangd_flags)
+end
+
+local custom_on_init = function(client, bufnr)
+  require("lvim.lsp").common_on_init(client, bufnr)
+  require("clangd_extensions.config").setup {}
+  require("clangd_extensions.ast").init()
+  vim.cmd [[
+  command ClangdToggleInlayHints lua require('clangd_extensions.inlay_hints').toggle_inlay_hints()
+  command -range ClangdAST lua require('clangd_extensions.ast').display_ast(<line1>, <line2>)
+  command ClangdTypeHierarchy lua require('clangd_extensions.type_hierarchy').show_hierarchy()
+  command ClangdSymbolInfo lua require('clangd_extensions.symbol_info').show_symbol_info()
+  command -nargs=? -complete=customlist,s:memuse_compl ClangdMemoryUsage lua require('clangd_extensions.memory_usage').show_memory_usage('<args>' == 'expand_preamble')
+  ]]
+end
+
+local opts = {
+  cmd = { provider, unpack(clangd_flags) },
+  on_attach = custom_on_attach,
+  on_init = custom_on_init,
+}
+
+require("lvim.lsp.manager").setup("clangd", opts)
 
 -- install codelldb with :MasonInstall codelldb
 -- configure nvim-dap (codelldb)
@@ -55,7 +99,12 @@ lvim.builtin.dap.on_config_done = function(dap)
       type = "codelldb",
       request = "launch",
       program = function()
-        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        local path
+        vim.ui.input({ prompt = "Path to executable: ", default = vim.loop.cwd() .. "/build/" }, function(input)
+          path = input
+        end)
+        vim.cmd [[redraw]]
+        return path
       end,
       cwd = "${workspaceFolder}",
       stopOnEntry = false,
