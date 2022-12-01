ae259c136ce97959db8e231419072cff76c907ba
Aaron Coville <amcoville@gmail.com>
2022-11-30 20:16:44 -0500

    Remove deprecated lvim.builtin.notify.active (#52)
    The starter config in the java-side branch is toggling the lvim.builtin.notify.active setting which was deprecated as part of LunarVim#3294

--- config.lua
+++ config.lua
@@ -6,7 +6,6 @@ lvim.leader = "space"
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
