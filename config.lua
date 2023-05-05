a2573d3cb36e582f1e21e9d82a0186a64cf90616
Christian Chiarulli <chris.machine@pm.me>
2023-05-05 00:49:58 -0400

    update for lunarvim 1.3

--- config.lua
+++ config.lua
@@ -1,35 +1,5 @@
-lvim.log.level = "warn"
-lvim.format_on_save = false
-lvim.lsp.diagnostics.virtual_text = true
-lvim.leader = "space"
-
--- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
-lvim.builtin.breadcrumbs.active = true
-lvim.builtin.treesitter.highlight.enabled = true
-lvim.builtin.dap.active = true
-
--- if you don't want all the parsers change this to a table of the ones you want
-lvim.builtin.treesitter.ensure_installed = {
-  "java",
+lvim.plugins = {
+  "mfussenegger/nvim-jdtls",
 }
 
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "jdtls" })
-
--- set a formatter, this will override the language server formatting capabilities (if it exists)
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  { command = "google_java_format", filetypes = { "java" } },
-}
-
--- Additional Plugins
-lvim.plugins = {
-  "mfussenegger/nvim-jdtls",
-  "rcarriga/nvim-dap-ui"
-}
-
-require("user.dap-ui")
